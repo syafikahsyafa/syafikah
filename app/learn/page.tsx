6738abd0468a5a4f18124777dfa497a5d47d9db0
syafikahsyafa <laylaoff0@gmail.com>
2026-04-30 15:55:57 +0800

    update page dan tambah palette

---
+++ app/learn/page.tsx
@@ -0,0 +1,16 @@
+export default function LearnPage() {
+    return (
+    <div className="bg-white h-screen">
+        <div className="bg-purple-300 h-72 w-96 flex flex-col justify-between p-8 rounded-4xl">
+            <div className="flex gap-4">
+                <img src="/palette.svg"/>
+                <p>design</p>
+            </div>
+            <div>
+                 <h1 className="text-4xl font-bold"> Figma</h1>
+                <p>8 hours ago</p>
+            </div>
+        </div>
+    </div>
+    );  
+}
