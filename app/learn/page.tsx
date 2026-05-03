ba15eec072a1cfad75388063b0a1a3c011a88dd1
syafikahsyafa <laylaoff0@gmail.com>
2026-05-03 15:39:32 +0800

    update1

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,45 +1,94 @@
 export default function LearnPage() {
     return (
-    <div className="bg-white h-screen">
-        <div className="bg-purple-300 h-72 w-96 flex flex-col justify-between p-8 rounded-4xl text-blue-950">
+    <div className="bg-white h-screen flex items-center justify-center flrx itemns-between justify between">
+        <div className="bg-purple-300 h-60 w-60 flex flex-col justify-between p-8 rounded-4xl text-blue-950">
             <div className="flex gap-4">
                 <img src="/palette.svg"/>
                 <p>design</p>
             </div>
             <div>
-                 <h1 className="text-4xl font-bold"> Figma</h1>
-                <p className="text-lg">8 hours ago</p>
-
-                <div>
-
-                </div>
-
-        <div className="bg-orange-300 h-72 w-96 flex flex-col justify-between p-12 rounded-4xl text-blue-950">
-            <div className="flex gap-4">
-                <img src="/palette.svg"/>
-                <p>design</p>
-            </div>
-            <div>
-                 <h1 className="text-4xl font-bold"> Figma</h1>
-                <p className="text-lg">8 hours ago </p>
-
-        <div className="bg-red-300 h-72 w-96 flex flex-col justify-between p-8 rounded-4xl">
-            <div className="flex gap-4">
-                <img src="/palette.svg"/>
-                <p>design</p>
-            </div>
-            <div>
-                 <h1 className="text-4xl font-bold"> Figma</h1>
+                 <h1 className="text-3xl font-bold"> Figma</h1>
                 <p>8 hours ago</p>
             </div>
         </div>
-    </div>
-                
+
+                <div className="h-screen flex items-center justify-center">
+                    <div className="flex gap-4 px-4">
+                        <div className="flex-col justify-between">
+
+                        </div>
+                    </div>
+                </div>
+        <div className="bg-white h-screen flex items-center justify-center">        
+        <div className="bg-orange-200 h-60 w-60 flex flex-col justify-between p-8 rounded-4xl text-blue-950">
+            <div className="flex gap-4">
+                <img src="/code.svg"/>
+                <p>Codding</p>
+            </div>
+            <div>
+                 <h1 className="text-3xl font-bold"> Figma</h1>
+                <p>2 hours ago </p>
+        </div>
+        </div>
+
+                <div className="h-screen flex items-center justify-between">
+                    <div className="flex gap-4 px-4">
+
+                    </div>
+                </div>
+
+    <div className="bg-white h-screen flex items-center justify-center">
+        <div className="bg-red-200 h-60 w-60 flex flex-col justify-between p-8 rounded-4xl text-blue-950">
+            <div className="flex gap-4">
+                <img src="/palette.svg"/>
+                <p>design</p>
+            </div>
+            <div>
+                 <h1 className="text-3xl font-bold">Sketch</h1>
+                <p>in 4 days </p>
             </div>
         </div>
     </div>
 
+        <div className="bg-white h-screen p-10 flex flex-wrap items-start gap-6">
+            <div className="bg-blue-200 h-80 w-90 flex flex-col justify-between p-8 rounded-4xl text-blue-950">
+            <div className="flex gap-4">
+                 <img src="/palette.svg"/>
+                 <p>Design</p>   
             </div>
+            <div>
+                <h1 className="text-3xl font-bold">Adobe Photoshop</h1>
+                <p>in 3 days</p>
+                <div className="flex -mt mt-3">
+                    <img src="/avatar1.jpg" className="w-8 h-8 rounded-full border-2 border-white -ml-2"/>
+                    <img src="/avatar2.jpg" className="w-8 h-8 rounded-full border-2 border-white -ml-2"/>
+                    <img src="/avatar3.jpg" className="w-8 h-8 rounded-full border-2 border-white -ml-2"/>
+                </div>
+            </div>
+            </div>
+        </div>
+
+        <div className="bg-white h-screen flex items-center justify-between">
+            <div className="bg-green-200 h-80 w-90 flex flex-col justify-between p-8 rounded-4xl text-blue-950">
+            <div className="flex gap-4">
+                    <img src="/palette.svg"/>
+                    <p>AI</p>
+            </div>
+            <div>
+                <h1 className="text-3xl font-bold">DALL E 2, Midjourney, Stable Diffusion</h1>
+                <p>in 5 days</p>
+                <div className="flex -mt mt-3">
+                    <img src="/avatar1.jpg" className="w-8 h-8 rounded-full border-2 border-white -ml-2"/>
+                    <img src="/avatar2.jpg" className="w-8 h-8 rounded-full border-2 border-white -ml-2"/>
+                    <img src="/avatar3.jpg" className="w-8 h-8 rounded-full border-2 border-white -ml-2"/>
+                </div>
+            </div>
+            </div>
+         </div>
+
+        </div>
+    </div>
+    
 
     );  
 }
